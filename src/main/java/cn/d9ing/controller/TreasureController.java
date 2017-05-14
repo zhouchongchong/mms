c0fd83a5ade2b434b1de11581daabbc80c915a37
zhouchongchong <764098483@qq.com>
2017-05-14 18:33:27 +0800

    一些主要接口

--- src/main/java/cn/d9ing/controller/TreasureController.java
+++ src/main/java/cn/d9ing/controller/TreasureController.java
@@ -1,19 +1,25 @@
 package cn.d9ing.controller;
 
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
+import cn.d9ing.domain.Treasure;
 import cn.d9ing.service.ITreasureService;
+import cn.d9ing.utils.DateUtils;
+import cn.d9ing.utils.Keys;
 import cn.d9ing.utils.UploadUtils;
 import cn.d9ing.utils.beans.FileBean;
 
@@ -41,4 +47,64 @@ public class TreasureController {
 		return pathMap;
 	}
 	
+	/**  
+	 * addTreasure:新增 藏品. <br/>    
+	 * @author zhouchong  
+	 * @param request
+	 * @param treasure
+	 * @since JDK 1.8  
+	 */
+	 @RequestMapping("/addtreasure")
+	 @ResponseBody
+	public Integer insertTreasure(HttpServletRequest request,Treasure treasure){
+		try {
+			Date date = DateUtils.sqlDate();
+			treasure.settCreatetime(date);
+			treasure.settUptime(date);
+			treasure.setIsdelte(Keys.IS_NOT_DELETE);
+			Integer result =  treasureService.insertTreasureSelective(treasure);
+			return result;
+		} catch (Exception e) {
+			e.printStackTrace(); 
+			return 0;
+		}
+	}
+	 
+	/**  
+	 * getCurrtPageTreasure:页查询. <br/>    
+	 * @author zhouchong  
+	 * @param request
+	 * @return  
+	 * @since JDK 1.8  
+	 */
+	 @RequestMapping("/pagetreasure")
+	 @ResponseBody
+	public List<Treasure> getCurrtPageTreasure(HttpServletRequest request,Integer page,Integer pageSize,Integer dynasty){
+		if (page <= 0 && pageSize <=0 ){
+			return null;
+		}
+		return treasureService.searchTreasurePage(page, pageSize, dynasty);
+	}
+	
+	/**  
+	 * getTresureById:详情页. <br/>    
+	 * @author zhouchong  
+	 * @param request
+	 * @param tId
+	 * @return  
+	 * @since JDK 1.8  
+	 */
+	 @RequestMapping("/gettreasurebyid")
+	public String getTresureById(HttpServletRequest request, Long tId,Model model){
+		if (tId <= 0) {
+			return null;
+		}
+		Treasure treasure = treasureService.selectTresureById(tId);
+		model.addAttribute("treasure",treasure);
+		Treasure upTreasure = treasureService.getTreasureNumUp(tId, treasure.gettDynasty());
+		Treasure downTreasure = treasureService.getTreasureNumDown(tId, treasure.gettDynasty());
+		model.addAttribute("upTreasure",upTreasure);
+		model.addAttribute("downTreasure",downTreasure);
+		return "";
+	}
 }
